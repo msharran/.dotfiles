18177fb350d322e53166bb35c5144ab0799b673d
Sharran <sharran.murali@gmail.com>
2024-06-09 00:32:06 +0530

    tmux copy mode + catpuccin

--- .config/nvim/lua/msharran/plugins/ui.lua
+++ .config/nvim/lua/msharran/plugins/ui.lua
@@ -6,8 +6,10 @@ return {
             require('lualine').setup({
                 options = {
                     theme = 'catppuccin',
-                    component_separators = '',
                     section_separators = { left = "", right = "" },
+                    icons_enabled = true,
+                    component_separators = { left = '|', right = '|' },
+                    always_divide_middle = true
                 },
                 sections = {
                     -- left
@@ -16,7 +18,34 @@ return {
                     lualine_c = { 'filename' },
 
                     -- right
-                    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                    lualine_x = {
+                        {
+                            'copilot',
+                            -- Default values
+                            symbols = {
+                                status = {
+                                    icons = {
+                                        enabled = " ",
+                                        sleep = " ", -- auto-trigger disabled
+                                        disabled = " ",
+                                        warning = " ",
+                                        unknown = " "
+                                    },
+                                    hl = {
+                                        enabled = "#50FA7B",
+                                        sleep = "#AEB7D0",
+                                        disabled = "#6272A4",
+                                        warning = "#FFB86C",
+                                        unknown = "#FF5555"
+                                    }
+                                },
+                                spinners = require("copilot-lualine.spinners").dots,
+                                spinner_color = "#6272A4"
+                            },
+                            show_colors = true,
+                            show_loading = true
+                        },
+                        'encoding', 'fileformat', 'filetype' },
                     lualine_y = { 'progress' },
                     lualine_z = { 'location' }
                 },
@@ -24,6 +53,7 @@ return {
             })
         end
     },
+    { 'AndreM222/copilot-lualine' },
     {
         "norcalli/nvim-colorizer.lua",
         config = function()
@@ -32,7 +62,7 @@ return {
     },
     {
         'MeanderingProgrammer/markdown.nvim',
-        name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+        name = 'render-markdown',     -- Only needed if you have another plugin named markdown.nvim
         dependencies = { 'nvim-treesitter/nvim-treesitter' },
         config = function()
             require('render-markdown').setup({})
