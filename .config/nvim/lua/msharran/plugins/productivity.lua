33230483dabdb4db684318b2af3947131b6cfeac
Sharran <sharran.murali@gmail.com>
2024-06-09 20:34:13 +0530

    fzf telescope

--- .config/nvim/lua/msharran/plugins/productivity.lua
+++ .config/nvim/lua/msharran/plugins/productivity.lua
@@ -44,14 +44,48 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.6',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+        },
         config = function()
+            require('telescope').setup {
+                extensions = {
+                    fzf = {
+                        fuzzy = true, -- false will only do exact matching
+                        override_generic_sorter = true, -- override the generic sorter
+                        override_file_sorter = true, -- override the file sorter
+                        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                        -- the default case_mode is "smart_case"
+                    }
+                }
+            }
+            require('telescope').load_extension('fzf')
+
+            -- telescope keymaps
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
+            vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fw', function()
+                local word = vim.fn.expand("<cword>")
+                if word == "" then
+                    print("No word under cursor")
+                    return
+                end
+                builtin.grep_string({ search = vim.fn.input("Grep > ", word) })
+            end)
+            vim.keymap.set('n', '<leader>fW', function()
+                local word = vim.fn.expand("<cWORD>")
+                if word == "" then
+                    print("No word under cursor")
+                    return
+                end
+                builtin.grep_string({ search = vim.fn.input("Grep > ", word) })
+            end)
         end
     },
-    { "christoomey/vim-tmux-navigator" }
+    { "christoomey/vim-tmux-navigator" },
 }
