09e0fcc70ece58e225defc6bacd4b6d94aa0c10e
Sharran <sharran.murali@gmail.com>
2024-08-03 19:24:42 +0530

    fish abbr git

--- .config/fish/conf.d/3000-abrrs.fish
+++ .config/fish/conf.d/3000-abrrs.fish
@@ -3,7 +3,9 @@ abbr --add gst git status
 abbr --add gco git checkout
 abbr --add gcmsg git commit -m
 abbr --add ga git add
+abbr --add gaa git add .
 abbr --add ggp git push 
 abbr --add gpr git pull --rebase
 abbr --add gw git worktree
 abbr --add glog git log --graph --oneline
+abbr --add grhh git reset --hard HEAD
