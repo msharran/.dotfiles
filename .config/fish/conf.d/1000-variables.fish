7b70e59384c82776874e6b1a515fb0e051538699
Sharran <sharran.murali@gmail.com>
2024-08-11 00:39:01 +0530

    fish path

--- .config/fish/conf.d/1000-variables.fish
+++ .config/fish/conf.d/1000-variables.fish
@@ -52,3 +52,6 @@ set -gx FZF_DEFAULT_OPTS "--height 40% --layout reverse --border"
 # Brew luarocks
 # export DYLD_LIBRARY_PATH="$(brew --prefix)/lib:$DYLD_LIBRARY_PATH"
 set -gx DYLD_LIBRARY_PATH "$(brew --prefix)/lib" $DYLD_LIBRARY_PATH
+
+# Rust
+fish_add_path $HOME/.cargo/bin
