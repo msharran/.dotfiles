d9e04c0002dd4a07e10c4d2f0c62cf12a11e4324
msharran <sharran.murali@gmail.com>
2024-08-22 15:25:20 +0000

    nix

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    git
+    neovim 
+    lua 
+    luajit 
+    rustc 
+    cargo 
+    nodejs_22
+    go
+  ];
+
+  shellHook = ''
+    fish 
+  '';
+}
