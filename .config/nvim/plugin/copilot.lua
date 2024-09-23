b96514e93fe8d45e2e005ccc3dab09990d5030ce
Sharran <sharran.murali@gmail.com>
2024-09-23 15:58:49 +0530

    kitty

--- .config/nvim/plugin/copilot.lua
+++ .config/nvim/plugin/copilot.lua
@@ -1,20 +1,20 @@
-require('copilot').setup({
-    panel = { enabled = true },
-    suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-            accept = "<C-b><C-y>",
-            accept_word = false,
-            accept_line = false,
-            next = "<C-b><C-n>",
-            prev = "<C-b><C-p>",
-            dismiss = "<C-b><C-c>",
-        },
-    },
-    filetypes = {
-        yaml = true,
-        markdown = true,
-    },
-})
+-- require('copilot').setup({
+--     panel = { enabled = false },
+--     suggestion = {
+--         enabled = true,
+--         auto_trigger = true,
+--         debounce = 75,
+--         keymap = {
+--             accept = "<C-b><C-y>",
+--             accept_word = false,
+--             accept_line = false,
+--             next = "<C-b><C-n>",
+--             prev = "<C-b><C-p>",
+--             dismiss = "<C-b><C-c>",
+--         },
+--     },
+--     filetypes = {
+--         yaml = true,
+--         markdown = true,
+--     },
+-- })
