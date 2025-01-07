44b83761fb88b9bf8df0cca8996e94e2fd19a5b8
Sharran <sharran.murali@gmail.com>
2025-01-07 14:58:47 +0530

    eza fish abbr

--- .config/fish/conf.d/shell.fish
+++ .config/fish/conf.d/shell.fish
@@ -1,10 +1,4 @@
 # If hyperlinks are printed, kitty can use hint kitten to
 # take some actions on them like opening it in a browser
-function l
-    # if gls is in PATH, use it, else fallback to ls
-    if command -v eza > /dev/null 
-        eza --hyperlink -l $argv
-    else
-        ls $argv
-    end
-end
+
+abbr --add l eza --hyperlink -l --icons
