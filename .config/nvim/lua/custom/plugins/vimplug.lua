3a497834e4ca885006e2cce4f3d48c728c3b9698
Sharran <sharran.murali@gmail.com>
2025-01-09 21:37:53 +0530

    ctrl p

--- .config/nvim/lua/custom/plugins/vimplug.lua
+++ .config/nvim/lua/custom/plugins/vimplug.lua
@@ -8,6 +8,7 @@ Plug 'tpope/vim-surround'    " Surround text with brackets, quotes, etc.
 Plug 'tpope/vim-unimpaired'  " Pairs of useful mappings
 Plug 'tpope/vim-sensible'    " Sensible defaults 
 Plug 'numToStr/Comment.nvim' " Comment lines
+Plug 'ctrlpvim/ctrlp.vim'    " Fuzzy file finder
 
 " Looks
 Plug 'ellisonleao/gruvbox.nvim'
